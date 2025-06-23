7768e715445638a7dcde8c8921b6216a1511f2a5
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-06-23 17:57:56 -0500

    constructor

--- BTree.java
+++ BTree.java
@@ -3,4 +3,9 @@ public class BTree<E extends Comparable<E>> {
     private int orden;
     private boolean up;
     private BNode<E> nDes;
+
+    public BTree(int orden){
+        this.orden = orden;
+        this.root = null;
+    }
 }
