dcab0e5d072a52bd86a8d0714a7d0202007d9ff7
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-06-23 23:52:13 -0500

    try main

--- arbol/Main.java
+++ arbol/Main.java
@@ -1,5 +1,11 @@
 package arbol;
+import excepciones.ItemNoFound;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        try {
+            String ruta = "arbolB.txt";
+            
+
+    }
 }
