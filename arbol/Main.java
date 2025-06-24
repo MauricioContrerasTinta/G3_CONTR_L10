4441f941d0e058ff31e294174b65444cf893e7ea
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-06-23 23:54:08 -0500

    Agregar construcción y prueba de búsqueda del BTree en el método main

--- arbol/Main.java
+++ arbol/Main.java
@@ -5,7 +5,15 @@ public class Main {
     public static void main(String[] args) {
         try {
             String ruta = "arbolB.txt";
+            BTree<Integer> arbol = BTree.building_Btree(ruta);
             
+            System.out.println("Arbol B construido:");
+            System.out.println(arbol.toString());
 
+            // Prueba de búsqueda
+            System.out.println("\n¿Existe 19?: " + arbol.search(19));
+            System.out.println("¿Existe 100?: " + arbol.search(100));
+
+        }
     }
 }
