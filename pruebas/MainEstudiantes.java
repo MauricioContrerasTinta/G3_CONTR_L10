57b112345fec11b7afadff83962350adc3c91267
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-06-24 00:21:40 -0500

    Agregar método main para inicializar el árbol B en MainEstudiantes

--- pruebas/MainEstudiantes.java
+++ pruebas/MainEstudiantes.java
@@ -3,6 +3,9 @@ import arbol.BTree;
 import modelo.RegistroEstudiante;
 
 public class MainEstudiantes {
-
+    public static void main(String[] args) {
+        //crear arbol B de orden 4 que almacena objetos de tipo RegistroEstudiante
+        BTree<RegistroEstudiante> arbol = new BTree<>(4);
+    }
 }
 
